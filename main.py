9ebe74913f5dc08448c4a88c9abb29150b40543b
Adam Altice <>
2022-09-30 17:20:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Project 1
+#Adam Altice
+#Software Foundations
+
+#Welcome Prompt
+title = "Welcome to Adam's Awesome Car Shop!"
+#e1 = []
+#for a in title:
+#  e1.append("*")
+#print(e1)
+#print(title)
+#print(e1)
+
+# Set length of border to title length for clean look
+x = len(title)
+titleBorder = "*" * x
+print(titleBorder)
+print(title)
+print(titleBorder)
+
